0917ff8f55b090291ab5ff1a07bd2541395372af
Jesse Lochness <worldstagemusicapp@gmail.com>
2023-04-16 13:24:24 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,3 +1,5 @@
+import { Text } from "react-native";
+import { CheckBox } from "react-native-elements";
 import { ImageBackground } from "react-native";
 import React from "react";
 import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
@@ -9,8 +11,8 @@ const Untitled2 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><View style={styles.tWOjLdCg}><ImageBackground style={styles.OoUoCfNe} source={require("./logingoogle.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.wNbSBAkC} source={require("./loginemail.png")} resizeMode="cover"><ImageBackground style={styles.GiFJsCMf} source={require("./loginterms.png")} resizeMode="cover"></ImageBackground></ImageBackground><ImageBackground style={styles.IGOjqhZp} source={require("./loginmember.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.yWxiZNbM} source={require("./loginmusicnoyeblack.png")} resizeMode="cover"></ImageBackground><ImageBackground style={styles.VJqHyBht} source={require("./loginwsm.png")} resizeMode="cover"></ImageBackground></View></ScrollView>
-    </SafeAreaView>;
+    }}><View style={styles.tWOjLdCg}><ImageBackground style={styles.OTsSlGcj} source={require("./loginwsm.png")} resizeMode="cover"></ImageBackground><CheckBox style={styles.JdiaxtPN} title="Remember me" size={16}></CheckBox><View style={styles.nifyCQXr}></View><Text style={styles.fOqkEFzB}>Username/Email</Text><Text style={styles.SyqawwLn}>Password:</Text></View></ScrollView>
+    <View style={styles.ZtOYroPz}></View></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
@@ -31,8 +33,8 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     color: "#777777",
     position: "absolute",
-    left: 1,
-    top: 2,
+    left: 3,
+    top: 3,
     borderWidth: 4
   },
   YFiswxSL: {
@@ -81,8 +83,8 @@ const styles = StyleSheet.create({
     width: 318,
     height: 103,
     position: "absolute",
-    left: 10,
-    top: 171
+    left: 3,
+    top: 425
   },
   VJqHyBht: {
     width: 332,
@@ -90,6 +92,136 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: 7.5,
     top: 33
+  },
+  EqhKRyzh: {
+    width: 328,
+    height: 328,
+    position: "absolute",
+    left: 5,
+    top: 4
+  },
+  TVchFDmk: {
+    width: 297,
+    height: 288,
+    position: "absolute",
+    left: 19,
+    top: 16
+  },
+  ryitVCHf: {
+    width: 315,
+    height: 295,
+    position: "absolute",
+    left: 10,
+    top: 18
+  },
+  GoufaVwC: {
+    width: 461,
+    height: 138,
+    position: "absolute",
+    left: -169,
+    top: 136
+  },
+  uCrlxcUz: {
+    width: 317,
+    height: 312
+  },
+  OTsSlGcj: {
+    width: 170,
+    height: 170,
+    position: "absolute",
+    left: 88.5,
+    top: 48
+  },
+  BTUEFVKC: {
+    width: 336,
+    height: 79,
+    position: "absolute",
+    left: 5.5,
+    top: 301
+  },
+  DbfkFGEl: {
+    width: 333,
+    height: 75,
+    position: "absolute",
+    left: 2,
+    top: 65
+  },
+  KlDfHNjb: {
+    width: 334,
+    height: 66,
+    position: "absolute",
+    left: 6.5,
+    top: 454
+  },
+  VevXHxMl: {
+    width: 327,
+    height: 48,
+    position: "absolute",
+    left: 3.5,
+    top: 56
+  },
+  JdiaxtPN: {
+    width: 183,
+    height: 69,
+    position: "absolute",
+    left: 82,
+    top: 462
+  },
+  nifyCQXr: {
+    height: 33,
+    width: 236,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 11,
+    color: "#777777",
+    position: "absolute",
+    left: 55.5,
+    top: 366
+  },
+  BUFMSqDY: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 167,
+    height: 30,
+    position: "absolute",
+    left: 25,
+    top: 0
+  },
+  ZtOYroPz: {
+    height: 33,
+    width: 233,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 11,
+    color: "#777777",
+    position: "absolute",
+    left: 64,
+    top: 432
+  },
+  fOqkEFzB: {
+    width: 114,
+    height: 21,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    left: 59,
+    top: 343
+  },
+  SyqawwLn: {
+    width: 73,
+    height: 21,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    left: 62,
+    top: 403
+  },
+  fzRQINYI: {
+    width: 180,
+    height: 61,
+    position: "absolute",
+    left: 84,
+    top: 503
   }
 });
 export default Untitled2;
